67831fce669ba9808dd85e810d5bde0ccc0b890d
Matheus Nascimento <98470693+nmttw@users.noreply.github.com>
2025-10-21 10:55:30 -0300

    Update test_app.py

--- testes/test_app.py
+++ testes/test_app.py
@@ -2,5 +2,5 @@ from src.app import hello
 
 def test_hello():
     resultado = hello()
-    print("Resultado da função hello():", resultado)
+    print(\n"Resultado da função hello():", resultado)
     assert hello() == "Desafio DevOps rodando com sucesso!"
