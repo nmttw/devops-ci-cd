fc60f7d88aff66225a5fc60f401b70e60ce93bd3
Matheus Nascimento <98470693+nmttw@users.noreply.github.com>
2025-10-21 14:03:05 +0000

    Refactor: Implementação de espaçamento

--- testes/test_app.py
+++ testes/test_app.py
@@ -2,5 +2,5 @@ from src.app import hello
 
 def test_hello():
     resultado = hello()
-    print(\n"Resultado da função hello():", resultado)
+    print(  "\nResultado da função hello():", resultado)
     assert hello() == "Desafio DevOps rodando com sucesso!"
